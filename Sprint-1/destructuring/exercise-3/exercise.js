eeefda373b0df32f642720f3212d21e5a09a6e08
khalidbih <kain19m@hotmail.com>
2026-04-05 14:02:47 +0100

    Complete exercise-3 print order receipt with destructuring

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,20 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+console.log("QTY     ITEM          TOTAL");
+
+let grandTotal = 0;
+
+order.forEach((item) => {
+  const { itemName, quantity, unitPricePence } = item;
+
+  const totalPrice = (unitPricePence * quantity) / 100;
+
+  grandTotal += totalPrice;
+
+  console.log(
+    `${quantity}\t${itemName.padEnd(18, " ")}${totalPrice.toFixed(2)}`
+  );
+});
+
+console.log(`\nTotal: ${grandTotal.toFixed(2)}`);
