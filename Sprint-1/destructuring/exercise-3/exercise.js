0cf4fad5d2e3c3ff6fcf66d5b71eadd2457b6e12
khalidbih <kain19m@hotmail.com>
2026-04-10 16:34:57 +0100

    fixed the console output formatting

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -1,12 +1,12 @@
 let order = [
-  { itemName: "Hot cakes", quantity: 1, unitPricePence: 232 },
+  { itemName: "Hot Cakes", quantity: 1, unitPricePence: 232 },
   { itemName: "Apple Pie", quantity: 2, unitPricePence: 139 },
   { itemName: "Egg McMuffin", quantity: 1, unitPricePence: 280 },
   { itemName: "Sausage McMuffin", quantity: 1, unitPricePence: 300 },
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
-console.log("QTY     ITEM          TOTAL");
+console.log("QTY     ITEM                TOTAL");
 
 let grandTotal = 0;
 
@@ -18,7 +18,7 @@ order.forEach((item) => {
   grandTotal += totalPrice;
 
   console.log(
-    `${quantity}\t${itemName.padEnd(18, " ")}${totalPrice.toFixed(2)}`
+    `${String(quantity).padEnd(8, " ")}${itemName.padEnd(18, " ")}${totalPrice.toFixed(2).padStart(6, " ")}`
   );
 });
 
